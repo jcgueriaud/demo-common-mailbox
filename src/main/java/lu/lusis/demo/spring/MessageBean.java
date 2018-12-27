5d503922c7c9c1f040c76d461488ca7f9e118547
jcgueriaud <jeanchristophe.gueriaud@gmail.com>
2018-12-27 18:02:02 +0100

    Initial version

---
+++ src/main/java/lu/lusis/demo/spring/MessageBean.java
@@ -0,0 +1,12 @@
+package lu.lusis.demo.spring;
+
+import java.time.LocalTime;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageBean {
+
+    public String getMessage() {
+        return "Button was clicked at " + LocalTime.now();
+    }
+}
